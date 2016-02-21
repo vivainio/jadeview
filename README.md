d098e22999e888472f31b19feb21ec34fa59c324
Ville M. Vainio <ville.vainio@basware.com>
2016-02-21 17:42:11 +0200

    readme, strip boilerplate comments

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-# README
-## This is the README for your extension "jadeview" 
-You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+# JadeView
 
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
+Visual Studio Code extension to view current HTML file in
+[Jade](http://jade-lang.com/) format. Useful for analyzing complex HTML files
+without being distracted by the tag soup.
 
-### For more information
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-** Enjoy!**
+License: MIT.
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,4 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 
@@ -10,19 +8,9 @@ const tmp = require('tmp');
 import path = require('path');
 import fs = require('fs');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "jadeview" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.jadeView', () => {
-        // The code you place here will be executed every time your command is executed
-
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
@@ -42,13 +30,11 @@ export function activate(context: vscode.ExtensionContext) {
                     vscode.window.showTextDocument(doc);
                 });
             });
-
         });
     });
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
